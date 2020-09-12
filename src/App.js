1b64f8e00a29b6e185ccb01ffc8ef9e81ec5e453
Rohan Julka <rohanjulka19@gmail.com>
2020-09-12 20:57:09 +0530

    Retrieves tweets from twitter-api-handler

--- src/App.js
+++ src/App.js
@@ -2,90 +2,61 @@ import React from 'react'
 import Main from './components/main'
 import Sidebar from './components/sidebar'
 import './App.css'
-import {twitter_provider,auth_handler} from './firebase.js'
-/*import crypto from 'crypto'
-import OAuth from 'oauth-1.0a'
+import { twitter_provider, auth_handler } from './firebase.js'
 import axios from 'axios'
-import { METHODS } from 'http'*/
 
 class App extends React.Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
-        users : [{
-          accessToken : "2447266872-owizz1VtMUlozSqW685ZlPYo6FlnfFxnJurlxOw",
-          secret: "PhHMi1qVRALypOx4gY2wl60Vs99nzz3sjIqQppAoc3nlX",
-          providerData : {
-            displayName: "rohan",
-            photoURL: "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
-            providerId: "twitter.com" ,
-            uid: "2447266872"
-          }
-        }]
+      users: [/*{
+       accessToken: "2447266872-owizz1VtMUlozSqW685ZlPYo6FlnfFxnJurlxOw",
+        secret: "PhHMi1qVRALypOx4gY2wl60Vs99nzz3sjIqQppAoc3nlX",
+        providerData: {
+          displayName: "rohan",
+          photoURL: "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+          providerId: "twitter.com",
+          uid: "2447266872"
+        }
+      }*/]
     }
     this.addAccount = this.addAccount.bind(this)
-    this.getTweets = this.getTweets.bind(this)
   }
 
   addAccount() {
 
     auth_handler.signInWithPopup(twitter_provider).then((result) => {
-      
-      this.setState( {
-          users : [...this.state.users,{
+
+      this.setState({
+        users: [...this.state.users, {
           accessToken: result.credential.accessToken,
           secret: result.credential.secret,
           providerData: result.user.providerData[0]
-      }]
-    })
-        console.log(result)
-        console.log("Success")
+        }]
+      })
+      axios.post("http://localhost:8000/user", {
+        key: result.credential.accessToken,
+        secret: result.credential.secret
+      })
+      console.log(result)
+      console.log("Success")
     }).catch((error) => {
       console.log(error)
     })
   }
 
-  getTweets() {
- /*   const oauth = OAuth ({
-      consumer: {key : "L9ecKX1gF1zti6iLYUltnMjPJ", secret: "ux6E5MV0x7FSZzjQRTFVgT6KG3bez3lKOOHOgpTUGfXLBd7mAG"} ,
-      signature_method: 'HMAC-SHA1',
-      hash_function(base_string, key) {
-        return crypto
-              .createHmac('sha1', key)
-              .update(base_string)
-              .digest('base64')
-      },
-    })
-
-    const request = {
-      url:'https://api.twitter.com/1.1/statuses/home_timeline.json',
-      method:'get',
-    }
-    const token = {
-      key: "2447266872-owizz1VtMUlozSqW685ZlPYo6FlnfFxnJurlxOw", 
-      secret : "PhHMi1qVRALypOx4gY2wl60Vs99nzz3sjIqQppAoc3nlX"
-    }
-    axios.defaults.headers.common['Acess-Control-Allow-Oigin'] = "*"
-    axios.get(request.url,{
-      headers: oauth.toHeader(oauth.authorize(request, token))
-    }).then(result => {
-      console.log(result)
-    }).catch(error => {
-      console.log(error)
-    })
-    */
-  }
-  
-
   render() {
+    let targetComponent = <Main/> 
+    if(this.state.users.length === 0) 
+        targetComponent = <div className = "main"><h1>Add Account</h1></div>
     return (
-      <div className = "app">
-        <Sidebar onClick = {this.addAccount} users = {this.state.users} />
-        <Main  getTweets = {this.getTweets} />
+      <div className="app">
+        <Sidebar onClick={this.addAccount} users={this.state.users} />
+        {targetComponent}
       </div>
     )
   }
 }
 
-export default App ;
+export default App;
--- src/components/main.js
+++ src/components/main.js
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useInterval } from 'react'
 import TweetCard from './tweetcard'
 import '../App.css'
+import axios from 'axios'
 
 class Main extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            tweets : [{
+            tweets: [{
                 text: "To make room for more expression we will now count all emojis as equal—including those with gender‍‍‍ and skin t… https://t.co/MkGjXf9aXm",
-                id:"1050118621198921728",
+                id: "1050118621198921728",
                 created_at: this.getDateTime("Wed Oct 10 20:19:24 +0000 2018"),
-                user : {
+                user: {
                     name: "Twitter API",
                     screen_name: "TwitterAPI",
                     url: "https://t.co/8IkCzCDr19",
                     profile_image: "https://pbs.twimg.com/profile_images/942858479592554497/BbazLO9L_normal.jpg",
                     verified: true,
                 }
-            }]
+            }],
+            interval_id: ""
         }
+        this.getTweets = this.getTweets.bind(this)
+    }
+
+    componentDidMount() {
+        //this.getTweets()
+        let interval = setInterval(this.getTweets, 60000, true)
+        this.setState({
+            tweets: this.state.tweets,
+            interval_id: interval
+        })
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.interval_id)
     }
 
     getDateTime(date_time) {
@@ -27,15 +43,29 @@ class Main extends React.Component {
         let day = date_time_arr[0]
         let time = date_time_arr[3]
         let date = date_time_arr[2] + " " + date_time_arr[1] + " " + date_time_arr[5]
-        return ([day,date,time])
+        return ([day, date, time])
     }
 
+
+    getTweets() {
+        console.log("Getting Tweets")
+        axios.get('http://localhost:8000/tweets')
+            .then((result) => {
+                result.data.map((tweet)=>{
+                    this.setState({
+                        tweets : [...this.state.tweets,tweet]
+                    })
+                })
+            })
+    }
+
+
     render() {
-        let tweets = this.state.tweets.map((tweet)=> {
-            return <TweetCard tweet  = {tweet} />
+        let tweets = this.state.tweets.map((tweet) => {
+            return <TweetCard tweet={tweet} />
         })
         return (
-            <div className = "main">
+            <div className="main">
                 {tweets}
             </div>
         )
